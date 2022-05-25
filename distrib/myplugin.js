8fab70cfd2eed2d8341e24c82c0e47b526bbc495
MyName <MyEmail@domaine.lan>
2022-05-25 21:40:44 +0100

    Adding constuctor(), calc(), getResult()

--- distrib/myplugin.js
+++ distrib/myplugin.js
@@ -1,3 +1,13 @@
 class MyPlugin{
-    
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.result = null;
+    }
+    calc(){
+
+    }
+    getResult(){
+        
+    }
 }
